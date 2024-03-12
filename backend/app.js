53b3cf08d3df486c529c2d5331eaf0d814d53ae6
Nicolas <contact@nicolasrichelet.com>
2024-03-12 23:02:35 +0100

    Mise en place des 1eres routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const Book = require('./models/Book');
+const User = require('./models/User');
+
+mongoose.connect('mongodb+srv://user_admin:l7o5sgQoIaEq0Q0f@cluster0.aop5puc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  // { useNewUrlParser: true,
+    // useUnifiedTopology: true }
+)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
@@ -10,10 +21,32 @@ app.use((req, res, next) => {
     next();
   });
 
-app.get('/api/books', (req, res, next) => {
-    res.status(200).json(books);
-    next();
+app.post('/api/book', (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
+  });
+  Book.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistre !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
+app.put('/api/stuff/:id', (res, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  .then(book => res.status(200).json(book))
+  .catch(error => res.status(400).json({ error}));
+})
+
+app.get('/api/book', (req, res, next) => {
+  Book.find()
+  .then( book => res.status(200).json(book))
+  .catch(error => res.status(400).json({ error}));
+  });
+
+app.get('/api/book/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
 
 module.exports = app;
