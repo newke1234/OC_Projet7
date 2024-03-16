fd20ff55931afbc35f89fd40e08d152bddb1c063
Nicolas <contact@nicolasrichelet.com>
2024-03-16 20:34:38 +0100

    MAJ route user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userCtrl  =require('../controllers/user');
 
-const userCtrl = require('../controllers/user');
-
-router.post('/', userCtrl.createUser);
-
-// router.get('/:id', userCtrl.getOneUser);
-
-// router.put('/:id', userCtrl.modifyUser);
-
-// router.get('/' +
-//   '', userCtrl.getAllUser);
-
-// router.delete('/:id', userCtrl.deleteUser);
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
