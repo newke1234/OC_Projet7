490239e5a595ea1f7a6dd3cf16213c27375e9c10
Nicolas <contact@nicolasrichelet.com>
2024-03-19 22:32:25 +0100

    ajout  de la fonction d'authentifiaction sur les routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,22 +1,16 @@
 const express = require('express');
+const auth = require('auth');
 const router = express.Router();
 
 const bookCtrl = require('../controllers/book');
 
-router.get('/' +
-  '', bookCtrl.getAllBook);
-
-router.get('/:id', bookCtrl.getOneBook);
-
+router.get('/', auth, bookCtrl.getAllBook);
+router.get('/:id', auth, bookCtrl.getOneBook);
+router.post('/', auth, bookCtrl.createBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+// router.post('/:id', bookCtrl.rateBook);
 // router.get('/:id', bookCtrl.getBestRatingBook);
-
-router.post('/', bookCtrl.createBook);
-
-router.put('/:id', bookCtrl.modifyBook);
-
-router.delete('/:id', bookCtrl.deleteBook);
-
-// router.post('/:id', bookCtrl.rateBook)
 
 
 module.exports = router;
