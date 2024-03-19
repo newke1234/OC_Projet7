ca1a8f835505e386e0896b48191743b499d56c7c
Nicolas <contact@nicolasrichelet.com>
2024-03-19 23:51:09 +0100

    correct syntax file.mimetype

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
-        const extension = MIMI_TYPES[file:mimetype];
+        const extension = MIMI_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
 });
