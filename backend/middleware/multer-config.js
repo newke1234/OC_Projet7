caba587ce33f6b55e282d3dcc649866c9c080ab6
Nicolas <contact@nicolasrichelet.com>
2024-03-24 00:46:36 +0100

    Correction chemin images et correction nom de fichier (on enleve l'extension au debut du nom)

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -8,13 +8,15 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, '../images')
+        callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        let name = file.originalname.split(' ').join('_').split('.');
+        name.pop()
+        name = name.join('.')
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
 });
 
-module.exports = multer({ storage }).single('../images');
+module.exports = multer({ storage }).single('image');
