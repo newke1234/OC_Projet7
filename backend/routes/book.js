b1a5b2adafbcb899fe2f75622571210273d00f31
Nicolas <contact@nicolasrichelet.com>
2024-03-24 00:44:58 +0100

    ajout de multer dans la route post

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
+const multer = require('../middleware/multer-config');
 
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, bookCtrl.createBook);
+router.post('/', auth, multer, bookCtrl.createBook);
 router.put('/:id', auth, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 // router.post('/:id', bookCtrl.rateBook);
 // router.get('/:id', bookCtrl.getBestRatingBook);
-
 
 module.exports = router;
