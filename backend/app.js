a98fb0020a2bd9264bc008bd87c371ed6db051a0
Nicolas <contact@nicolasrichelet.com>
2024-03-30 18:12:24 +0100

    Changé l'ordre des fonctions

--- backend/app.js
+++ backend/app.js
@@ -8,23 +8,26 @@ const bookRoutes = require('./routes/book');
 
 dotenv.config();
 
-mongoose.connect(process.env.DATABASE)
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+// Middleware CORS
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 
 app.use(express.json());
 const path = require('path');
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/book', bookRoutes);
+app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+
+mongoose.connect(process.env.DATABASE)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 module.exports = app;
