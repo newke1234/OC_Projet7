42b418297f86626eee63df751b1526c9714ad419
Nicolas <contact@nicolasrichelet.com>
2024-03-30 18:16:11 +0100

    Ajout de la ligne "genre"

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -6,6 +6,7 @@ const bookSchema = mongoose.Schema({
   author: { type: String, required: true },
   imageUrl: { type: String, required: true },
   year: { type: Number, required: true },
+  genre: { type: String, required: true},
   ratings: [
     { userId: { type: String, required: true  },
       grade: {type: Number, required: true}
