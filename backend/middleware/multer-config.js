14d8d7901ff4cb5f9322ee17b4967f6ff9a72c8f
Nicolas <contact@nicolasrichelet.com>
2024-04-01 17:16:48 +0200

    Enlevé les options = On doit toujours mettre une image sinon erreur

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -19,84 +19,41 @@ const upload = multer({
     }
 }).single('image');
 
-const multer_builder = (image_optionelle) => {
-    if (image_optionelle) {
-        return (req, res, next) => {
-            upload(req, res, async (err) => {
-                if (err) {
-                    return res.status(400).json({ error: err.message });
-                }
-
-                if (req.file) {
-                    // Utiliser Sharp pour redimensionner et optimiser l'image
-                    try {
-                        const resizedImageBuffer = await sharp(req.file.buffer)
-                            .resize({ width: 800 }) // Redimensionner l'image en largeur 800px
-                            .webp({ quality: 50 }) // Définir la qualité de l'image webp
-                            .toBuffer();
-            
-                        // Enregistrer l'image redimensionnée sur le disque avec un nom unique
-                        let name = req.file.originalname
-                        .split(' ').join('_').split('.');
-                        name.pop();
-                        name = name.join('.');
-                            
-                        // Enregistrer l'image redimensionnée sur le disque avec un nom unique
-                        //  const extension = MIME_TYPES[file.mimetype];
-                        const filename = name + '_' + Date.now() + '.webp';
-                        fs.writeFileSync(`images/${filename}`, resizedImageBuffer);
-            
-                        // Mettre à jour la requête avec le nom de l'image redimensionnée
-                        req.file.filename = filename;
-                        
-                    } catch (error) {
-                        return res.status(500).json({ error: 'Erreur de traitement de l\'image' });
-                    }
-                }
-                next();
-            });
+module.exports = (req, res, next) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            return res.status(400).json({ error: err.message });
         }
-    } else {
-        return (req, res, next) => {
-            upload(req, res, async (err) => {
-                if (err) {
-                    return res.status(400).json({ error: err.message });
-                }
 
-                if (!req.file) {
-                    return res.status(400).json({ error: 'Aucun fichier selectionné' });
-                }
-        
-                // Utiliser Sharp pour redimensionner et optimiser l'image
-                try {
-                    const resizedImageBuffer = await sharp(req.file.buffer)
-                        .resize({ width: 800 }) // Redimensionner l'image en largeur 800px
-                        .webp({ quality: 50 }) // Définir la qualité de l'image webp
-                        .toBuffer();
-        
-                    // Enregistrer l'image redimensionnée sur le disque avec un nom unique
-                    let name = req.file.originalname
-                    .split(' ').join('_').split('.');
-                    name.pop();
-                    name = name.join('.');
-                        
-                     // Enregistrer l'image redimensionnée sur le disque avec un nom unique
-                    //  const extension = MIME_TYPES[file.mimetype];
-                     const filename = name + '_' + Date.now() + '.webp';
-                     fs.writeFileSync(`images/${filename}`, resizedImageBuffer);
-         
-                     // Mettre à jour la requête avec le nom de l'image redimensionnée
-                     req.file.filename = filename;
-                    
-                } catch (error) {
-                    return res.status(500).json({ error: 'Erreur de traitement de l\'image' });
-                }
-        
-                next();
-            });
-        };
-    }
+        if (!req.file) {
+            return res.status(400).json({ error: 'Aucun fichier selectionné' });
+        }
 
-}
+        // Utiliser Sharp pour redimensionner et optimiser l'image
+        try {
+            const resizedImageBuffer = await sharp(req.file.buffer)
+                .resize({ width: 800 }) // Redimensionner l'image en largeur 800px
+                .webp({ quality: 50 }) // Définir la qualité de l'image webp
+                .toBuffer();
 
-module.exports = multer_builder;
+            // Enregistrer l'image redimensionnée sur le disque avec un nom unique
+            let name = req.file.originalname
+            .split(' ').join('_').split('.');
+            name.pop();
+            name = name.join('.');
+                
+             // Enregistrer l'image redimensionnée sur le disque avec un nom unique
+            //  const extension = MIME_TYPES[file.mimetype];
+             const filename = name + '_' + Date.now() + '.webp';
+             fs.writeFileSync(`images/${filename}`, resizedImageBuffer);
+ 
+             // Mettre à jour la requête avec le nom de l'image redimensionnée
+             req.file.filename = filename;
+            
+        } catch (error) {
+            return res.status(500).json({ error: 'Erreur de traitement de l\'image' });
+        }
+
+        next();
+    });
+};
