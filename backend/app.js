1389515ec05ccd9f63329c04a7fa9106a2a75a6a
Nicolas <contact@nicolasrichelet.com>
2024-04-10 15:50:42 +0200

    + comments

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,40 @@
+// Importation du framework Express pour la création d'une application web
 const express = require('express');
+// Importation du module Mongoose pour l'interaction avec la base de données MongoDB
 const mongoose = require('mongoose');
 const app = express();
+// Importation du module dotenv pour charger les variables d'environnement à partir du fichier .env
 const dotenv = require('dotenv');
 
+// Importation des routes définies pour les utilisateurs et les livres
 const userRoutes = require('./routes/user');
 const bookRoutes = require('./routes/book');
 
 dotenv.config();
 
+// Construction de l'URL de connexion à la base de données MongoDB à partir des variables d'environnement
 const database_path = `${process.env.DB_HOST}${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}`;
+// Connexion à la base de données MongoDB
 mongoose.connect(database_path)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// Middleware CORS
-
+// Middleware CORS (Cross-Origin Resource Sharing) pour permettre les requêtes provenant de différents domaines
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Autoriser toutes les origines
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Définir les méthodes HTTP autorisées
   next();
 });
 
-
+// Utilisation du middleware intégré pour le traitement des données JSON dans les requêtes
 app.use(express.json());
+// Utilisation du middleware express.static pour servir les fichiers statiques (par exemple, ici les images) depuis le dossier 'images'
 const path = require('path');
-
 app.use('/images', express.static(path.join(__dirname, 'images')));
+// Utilisation des routes définies pour les livres et les utilisateurs
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
 
+// Exportation de l'application Express pour l'utiliser dans d'autres fichiers (par exemple, server.js)
 module.exports = app;
