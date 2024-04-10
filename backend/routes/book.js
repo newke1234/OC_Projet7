8f3dc099833f3c152aef6997b4e7ac63c6d30011
Nicolas <contact@nicolasrichelet.com>
2024-04-10 22:40:09 +0200

    + comments

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
-const multer = require('../middleware/multer-config');
+const multer = require('../middleware/multer-config'); // Importation du middleware multer pour la gestion des fichiers téléchargés
 
+// Importation du contrôleur de livres contenant la logique métier associée aux routes
 const bookCtrl = require('../controllers/book');
 
+// Définition des différentes routes pour les opérations CRUD sur les livres
 router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/', bookCtrl.getAllBook);
 router.get('/:id', bookCtrl.getOneBook);
